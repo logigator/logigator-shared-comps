98357b81ff6269013e891bfbe5e7095dbc40f029
Leo Preissegger <preissegger.leo@gmail.com>
2019-12-21 17:07:10 +0100

    add autoFontSize directive

---
+++ projects/logigator-shared-comps/src/lib/directives/auto-font-size/auto-font-size.directive.ts
@@ -0,0 +1,47 @@
+import {
+	AfterContentChecked,
+	Directive,
+	ElementRef,
+	Inject,
+	Input, OnChanges,
+	PLATFORM_ID, SimpleChanges
+} from '@angular/core';
+import {FontWidthService} from '../../services/font-width/font-width.service';
+
+@Directive({
+	selector: '[logiAutoFontSize]'
+})
+export class AutoFontSizeDirective implements AfterContentChecked, OnChanges {
+
+	@Input()
+	desiredFontWidth: number;
+
+	private oldText: string;
+
+	constructor(
+		private elementRef: ElementRef<HTMLElement>,
+		private fontWidthService: FontWidthService,
+		@Inject(PLATFORM_ID) private platformId: string,
+	) { }
+
+	ngAfterContentChecked(): void {
+		const currText = this.elementRef.nativeElement.innerText;
+		if (this.oldText !== currText) {
+			this.adjustFontSize();
+			this.oldText = currText;
+		}
+	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		this.adjustFontSize();
+	}
+
+	private adjustFontSize() {
+		const text = this.elementRef.nativeElement.innerText;
+		const style = window.getComputedStyle(this.elementRef.nativeElement);
+		const textWidth = this.fontWidthService.getTextWidth(text, style.font);
+
+		this.elementRef.nativeElement.style.fontSize = (Number(style.fontSize.replace('px', '')) * (this.desiredFontWidth / textWidth)) + 'px';
+	}
+
+}
